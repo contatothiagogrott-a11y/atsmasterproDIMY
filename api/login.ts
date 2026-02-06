cf64f04e08b3f17d253cd0bcacd124e5b8ca7392
contatothiagogrott-a11y <contato.thiagogrott@gmail.com>
2026-02-06 00:15:04 -0300

    Change runtime from 'edge' to 'nodejs'

--- api/login.ts
+++ api/login.ts
@@ -3,7 +3,7 @@ import { neon } from '@neondatabase/serverless';
 import { hash, compare } from 'bcryptjs';
 
 export const config = {
-  runtime: 'edge',
+  runtime: 'nodejs',
 };
 
 async function initTables(sql: any) {
